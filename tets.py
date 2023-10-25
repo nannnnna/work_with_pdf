0f96bb31efd9d07886b0746cf875d0957fe8f69e
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-10-25 12:44:55 +0300

    one file wuth all image

--- tets.py
+++ tets.py
@@ -1,14 +1,13 @@
 import fitz
 import os
+import io
 
-def extract_and_save_images_from_pdf_with_pymupdf(pdf_path, output_folder):
+from PIL import Image, ImageDraw, ImageFont
+
+def extract_and_save_images_from_pdf_with_pymupdf(pdf_path, output_file):
     pdf_document = fitz.open(pdf_path)
     
-    # Убедитесь, что папка для сохранения существует
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    image_links = []
+    images_to_merge = []
     
     for current_page in range(pdf_document.page_count):
         page = pdf_document.load_page(current_page)
@@ -19,17 +18,32 @@ def extract_and_save_images_from_pdf_with_pymupdf(pdf_path, output_folder):
             base_image = pdf_document.extract_image(xref)
             image_bytes = base_image["image"]
             
-            image_path = os.path.join(output_folder, f"page{current_page + 1}_img{image_index + 1}.png")
-            with open(image_path, "wb") as image_file:
-                image_file.write(image_bytes)
+            image = Image.open(io.BytesIO(image_bytes))
             
-            image_links.append(image_path)
+            # Добавление пометки о номере страницы
+            draw = ImageDraw.Draw(image)
+            text = f"Page {current_page + 1}"
+            font = ImageFont.truetype("arial.ttf", 15) # Указать путь к шрифту, если он отличается
+            draw.text((10,10), text, font=font, fill=(255,0,0,255))
             
-    return image_links
+            images_to_merge.append(image)
+
+    # Объединение всех изображений в одно
+    total_height = sum(i.height for i in images_to_merge)
+    max_width = max(i.width for i in images_to_merge)
+    combined_image = Image.new('RGB', (max_width, total_height))
+    
+    y_offset = 0
+    for im in images_to_merge:
+        combined_image.paste(im, (0, y_offset))
+        y_offset += im.height
+
+    combined_image.save(output_file)
+    return output_file
 
 pdf_path = 'admi.pdf'
-image_links = extract_and_save_images_from_pdf_with_pymupdf(pdf_path, 'output_images')
+output_file = 'combined_images.png'
+image_link = extract_and_save_images_from_pdf_with_pymupdf(pdf_path, output_file)
 
-# Вывод локальных "ссылок" на изображения
-for link in image_links:
-    print(link)
+# Вывод локальной "ссылки" на объединенное изображение
+print(image_link)
